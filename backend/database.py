8409106c4762b81ddf7251171482267e13c403b0
Rehan <77270784+rehanshami@users.noreply.github.com>
2025-03-31 16:43:44 +1300

    Update DB with Render Postgres

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres_data_ibla_user:ZPA4bDYPYgTrVoj7clC3mQ0ZWt3zYjY2@dpg-cvl0901r0fns7387p1ig-a.oregon-postgres.render.com/postgres_data_ibla"
+# Update with internal database URL
+DATABASE_URL = "postgresql://postgres_data_ibla_user:ZPA4bDYPYgTrVoj7clC3mQ0ZWt3zYjY2@dpg-cvl0901r0fns7387p1ig-a/postgres_data_ibla"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
